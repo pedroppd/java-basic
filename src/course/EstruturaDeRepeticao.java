70e8cd03709e26b979e9fae2d48781893b86b36f
Pedro Dantas <pedro_pdantas@hotmail.com>
2019-06-17 15:44:07 -0300

    Do while

--- src/course/EstruturaDeRepeticao.java
+++ src/course/EstruturaDeRepeticao.java
@@ -9,24 +9,36 @@ public class EstruturaDeRepeticao {
 
 		System.out.println("Digite um número: ");
 		int numberOne = sc.nextInt();
-		
-		while(numberOne<=0) {
+
+		while (numberOne <= 0) {
 			System.out.println("O número não pode ser negativo, tente denovo: ");
 			numberOne = sc.nextInt();
 		}
 
 		System.out.println("Digite quantos números você quer comparar: ");
 		int numberComparer = sc.nextInt();
-		
+
 		int total = 0;
 		int sum = 0;
-		for(int cont=1; cont<=numberComparer; cont++) {
+		for (int cont = 1; cont <= numberComparer; cont++) {
 			System.out.println("Digite um número");
 			total = sc.nextInt();
 			sum = sum += total;
 		}
-		System.out.println("Soma é: "+ sum);
-		
+		System.out.println("Soma é: " + sum);
+
+		System.out.println("----Raiz quadrada----");
+
+		char reply;
+		do {
+			System.out.println("Digite um número: ");
+			int number = sc.nextInt();
+			double rq = Math.sqrt(number);
+			System.out.printf("A raíz quadrada de é %.2f: ", rq);
+			System.out.print("Deseja continuar ? [s/n]");
+			reply = sc.next().charAt(0);
+
+		} while (reply != 'n');
 	}
 
 }
