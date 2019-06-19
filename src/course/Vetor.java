68f9cc912347cf732417584a9f70b74d2e1ce6e2
Pedro Dantas <pedro_pdantas@hotmail.com>
2019-06-19 14:16:13 -0300

    Vetor

---
+++ src/course/Vetor.java
@@ -0,0 +1,26 @@
+package course;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Vetor {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.println("Digite um número: ");
+		int n = sc.nextInt();
+		
+		double[] vect = new double[n];
+		
+		for(int i=0; i<vect.length;i++) {
+			vect[i] = sc.nextDouble();
+			System.out.println("vetor"+i+"-"+vect[i]);
+		}
+		
+
+	}
+
+}
